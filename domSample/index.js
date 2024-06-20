9a19ccd296839670bfc794ef267051e88bf7e238
Ashkar-m <151915974+Ashkar-m@users.noreply.github.com>
2024-06-20 20:00:55 +0530

    accept the text input and print it in another tag or field

--- domSample/index.js
+++ domSample/index.js
@@ -1,4 +1,21 @@
-let val=document.getElementById('head_val')
-val.innerHTML='hello I am just try to change the value by using get element by id!!!'
-val.style.color= 'red'
-val.style.background= 'green'
+// let val=document.getElementById('head_val')
+// val.innerHTML='hello I am just try to change the value by using get element by id!!!'
+// val.style.color= 'red'
+// val.style.background= 'green'
+
+// let val=document.getElementsByClassName('head_val')
+
+// val[0].innerHTML='I am just edited the document by using getelement by class'
+
+
+// get element by tag example
+
+// let val=document.getElementsByTagName('body')
+// val[0].style.background='red'
+// val[0].style.color='white'
+
+let val=document.getElementsByName('text')[0]
+let change=document.getElementById('values')
+function message(){
+    change.innerHTML=val.value
+}
